97af133efeb866c85e3d67dd850580061b243119
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-27 14:35:04 +0000

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,16 @@
 class Portfolio < ApplicationRecord
-    validates_presence_of :title, :body
+    validates_presence_of :title, :body, :main_image, :thumb_image
     
     def self.what
         where(body: 'What fjkdsal fhdslhs hahaha g elit, sed do eiusmod tempor.')
     end
     
     scope :ruby_on_rails_portfolio_items, -> { where(body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") }
+
+    after_initialize :set_defaults
+    
+    def set_defaults
+       self.main_image ||= "http://placehold.it/600x400"
+       self.thumb_image ||= "http://placehold.it/350x200"
+    end
 end
