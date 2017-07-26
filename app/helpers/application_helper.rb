bb9c6513700f95ad8617d10bde7b38a9a904785c
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-26 14:18:17 +0000

    Customized a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,7 +72,7 @@ module ApplicationHelper
     end
     
     def alert_generator msg
-        js add_gritter(msg, title: "Thor Madsen's Portfolio", sticky: false)
+        js add_gritter(msg, title: "Thor Madsen's Portfolio", sticky: false )
     end
     
 end
