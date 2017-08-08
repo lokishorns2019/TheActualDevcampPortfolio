bd57e9f3e6406187c8c00e773096f433df9fffe3
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-08-08 17:46:54 +0000

    Fixing errors

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 
-config.serve_static_assets = true
-
 Bundler.require(*Rails.groups)
 
 module AppIssac
