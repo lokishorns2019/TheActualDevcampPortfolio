c28c76a5e0d95f6a750c781a22113df590cd1b32
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-08-08 17:38:56 +0000

    Fixing login error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:change_language], unless: :devise_controller?
-  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   include CurrentUserConcern
   include DeviseWhitelistConcern
